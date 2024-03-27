07bfe51acb4fffb6747d7a1b3d2d75c3d8324dbc
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-03-27 22:00:25 +0545

    Update ordering_and_filtering.sql

--- !_Introduction_to_sql_server/ordering_and_filtering.sql
+++ !_Introduction_to_sql_server/ordering_and_filtering.sql
@@ -11,3 +11,16 @@ ORDER BY
 
 
 -- Modify your code based on the comments provided on the right.
+-- Select the top 20 rows from description, nerc_region and event_date
+SELECT 
+  TOP (20) description,
+  nerc_region,
+  event_date
+FROM 
+  grid 
+  -- Order by nerc_region, affected_customers & event_date
+  -- Event_date should be in descending order
+ORDER BY
+  nerc_region,
+  affected_customers,
+  event_date DESC;
