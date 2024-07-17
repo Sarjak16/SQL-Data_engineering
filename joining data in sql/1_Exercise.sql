2b4c3baaa1e7137b752d319556e5becdab697efa
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-07-17 21:05:54 +0545

    Update 1_Exercise.sql

--- joining data in sql/1_Exercise.sql
+++ joining data in sql/1_Exercise.sql
@@ -26,3 +26,19 @@ FROM cities
 INNER JOIN countries
 ON cities.country_code = countries.code;
 
+
+
+-- --4:
+-- Start with your inner join in line 5; join the tables countries AS c (left) with economies (right), aliasing economies AS e.
+-- Next, use code as your joining field in line 7; do not use the USING command here.
+-- Lastly, select the following columns in order in line 2: code from the countries table (aliased as country_code), name, year, and inflation_rate.
+
+--=>
+-- Select fields with aliases
+SELECT c.code AS country_code, name, year, inflation_rate
+FROM countries AS c
+-- Join to economies (alias e)
+INNER JOIN economies AS e
+
+-- Match on code field using table aliases
+ON c.code = e.code;
