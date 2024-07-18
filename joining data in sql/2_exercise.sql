5ef28472e15235bdd9278b2f031e476bcbd21863
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-07-18 21:12:49 +0545

    Update 2_exercise.sql

--- joining data in sql/2_exercise.sql
+++ joining data in sql/2_exercise.sql
@@ -1,4 +1,4 @@
---1.
+--1.1.
 -- Start with the join statement in line 6; perform an inner join with the countries table as c on the left with the languages table as l on the right.
 -- Make use of the USING keyword to join on code in line 8.
 -- Lastly, in line 2, select the country name, aliased as country, and the language name, aliased as language.
@@ -12,4 +12,27 @@ FROM countries AS c
 INNER JOIN languages AS l
 -- Use code as the joining field with the USING keyword
 USING (code);
+
+--1.2:
+-- Rearrange the SELECT statement so that the language column appears on the left and the country column on the right.
+-- Sort the results by language.
+-- Rearrange SELECT statement, keeping aliases
+SELECT c.name AS country, l.name AS language
+FROM languages AS l
+
+INNER JOIN countries AS c
+USING(code)
+-- Order the results by language
+ORDER BY language;
+
+--1.3:
+-- Select the incorrect answer from the following options.
+
+-- The query you generated in step 1 is provided below. Run this query (or the amendment you made in step 2) in the console to find the answer to the question.
+
+-- SELECT c.name AS country, l.name AS language
+-- FROM countries AS c
+-- INNER JOIN languages AS l
+-- USING(code)
+-- ORDER BY country;
 
