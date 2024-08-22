bdbca7466bdf198adcd49e295214e1a97da4e3aa
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-08-22 20:21:43 +0545

    Update 2_exercise.sql

--- joining data in sql/2_exercise.sql
+++ joining data in sql/2_exercise.sql
@@ -115,6 +115,14 @@ WHERE
    -- Define the order   
 ORDER BY 
   event_date DESC;
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
 
-
+-- Modify your query so that you are joining to economies on year as well as code.
+SELECT name, e.year, fertility_rate, unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+INNER JOIN economies AS e
+ON c.code = e.code
+-- Add an additional joining condition such that you are also joining on year
+AND  e.year = p.year;
