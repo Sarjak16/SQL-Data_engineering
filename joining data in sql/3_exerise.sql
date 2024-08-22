1dafaf31eb8d09cafd437c0594aae703d9d22f82
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-08-22 20:35:49 +0545

    Update 3_exerise.sql

--- joining data in sql/3_exerise.sql
+++ joining data in sql/3_exerise.sql
@@ -18,3 +18,14 @@ ON c1.country_code = c2.code
 -- 2
 -- Change the code to perform a LEFT JOIN instead of an INNER JOIN.
 -- After executing this query, have a look at how many records the query result contains.
+SELECT 
+	c1.name AS city, 
+    code, 
+    c2.name AS country,
+    region, 
+    city_proper_pop
+FROM cities AS c1
+-- Join right table (with alias)
+LEFT JOIN countries AS c2
+ON c1.country_code = c2.code
+ORDER BY code DESC;
