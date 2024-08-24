a9894b3a39bc7f0e48053522f295d5161f3789b9
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-08-24 20:56:42 +0545

    Update 2_exercise.sql

--- joining data in sql/2_exercise.sql
+++ joining data in sql/2_exercise.sql
@@ -126,3 +126,16 @@ INNER JOIN economies AS e
 ON c.code = e.code
 -- Add an additional joining condition such that you are also joining on year
 AND  e.year = p.year;
+
+
+
+-- .....................................................................................
+-- Complete the LEFT JOIN with the countries table on the left and the economies table on the right on the code field.
+-- Filter the records from the year 2010.
+SELECT name, region, gdp_percapita
+FROM countries AS c
+LEFT JOIN economies AS e
+-- Match on code fields
+USING(code) 
+-- Filter for the year 2010
+WHERE year=2010;
