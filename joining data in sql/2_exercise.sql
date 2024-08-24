627994b9b4f4f8675e02a4ed95b7f6d047bc73e1
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-08-24 21:04:32 +0545

    Update 2_exercise.sql

--- joining data in sql/2_exercise.sql
+++ joining data in sql/2_exercise.sql
@@ -129,7 +129,7 @@ AND  e.year = p.year;
 
 
 
--- .....................................................................................
+-- 1.....................................................................................
 -- Complete the LEFT JOIN with the countries table on the left and the economies table on the right on the code field.
 -- Filter the records from the year 2010.
 SELECT name, region, gdp_percapita
@@ -140,7 +140,7 @@ USING(code)
 -- Filter for the year 2010
 WHERE year=2010;
 
--- ........................................................................
+-- 2........................................................................
 -- To calculate per capita GDP per region, begin by grouping by region.
 -- After your GROUP BY, choose region in your SELECT statement, followed by average GDP per capita using the AVG() function, with AS avg_gdp as your alias.
 
@@ -154,6 +154,9 @@ WHERE year = 2010
 GROUP BY region;
 
 
+-- 3................................................................
+-- Order the result set by the average GDP per capita from highest to lowest.
+-- Return only the first 10 records in your result.
 
 
 SELECT region, AVG(gdp_percapita) AS avg_gdp
@@ -166,3 +169,4 @@ GROUP BY region
 ORDER BY avg_gdp DESC
 -- Return only first 10 records
 LIMIT 10;
+
