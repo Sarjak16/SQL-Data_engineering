7635afe72b7fb4391f377e0930c97c2491d50d3b
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-08-26 21:49:31 +0545

    Update 3_exerise.sql

--- joining data in sql/3_exerise.sql
+++ joining data in sql/3_exerise.sql
@@ -60,3 +60,34 @@ WHERE region = 'North America'
 	OR name IS NULL
 ORDER BY region;
 
+-- --3.
+-- Repeat the same query again, this time performing an inner join of countries with currencies.
+-- Have a look at what has changed in the output by comparing it to the full join and left join results!
+
+SELECT name AS country, code, region, basic_unit
+FROM countries
+-- Join to currencies
+INNER JOIN currencies 
+USING (code)
+WHERE region = 'North America' 
+	OR name IS NULL
+ORDER BY region;
+
+
+-- 4.
+-- Complete the FULL JOIN with countries as c1 on the left and languages as l on the right, using code to perform this join.
+-- Next, chain this join with another FULL JOIN, placing currencies on the right, joining on code again.
+SELECT 
+	c1.name AS country, 
+    region, 
+    l.name AS language,
+	basic_unit, 
+    frac_unit
+FROM countries as c1 
+-- Full join with languages (alias as l)
+FULL JOIN languages AS l
+USING (code)
+-- Full join with currencies (alias as c2)
+FULL JOIN currencies as c2 
+USING (code)
+WHERE region LIKE 'M%esia';
