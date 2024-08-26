0e2b12d53882ae4c56db4b12866b376380b953de
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-08-26 21:41:20 +0545

    Update 3_exerise.sql

--- joining data in sql/3_exerise.sql
+++ joining data in sql/3_exerise.sql
@@ -29,3 +29,21 @@ FROM cities AS c1
 LEFT JOIN countries AS c2
 ON c1.country_code = c2.code
 ORDER BY code DESC;
+
+
+
+-- FULL JOIN
+
+
+-- Perform a full join with countries (left) and currencies (right).
+-- Filter for the North America region or NULL country names.
+SELECT name AS country, code, region, basic_unit
+FROM countries
+-- Join to currencies
+FULL JOIN  currencies
+USING (code)
+-- Where region is North America or name is null
+WHERE region='North America'
+OR name IS NULL
+ORDER BY region;
+
