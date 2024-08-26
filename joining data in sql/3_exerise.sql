8141775e08cf9a43daaa01586fd2ecf76fdf147f
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-08-26 21:41:29 +0545

    Update 3_exerise.sql

--- joining data in sql/3_exerise.sql
+++ joining data in sql/3_exerise.sql
@@ -34,7 +34,6 @@ ORDER BY code DESC;
 
 -- FULL JOIN
 
-
 -- Perform a full join with countries (left) and currencies (right).
 -- Filter for the North America region or NULL country names.
 SELECT name AS country, code, region, basic_unit
